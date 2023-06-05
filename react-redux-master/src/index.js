06eb975e42b30984624e1d7bd7ea70d8d50ab0d1
AhmedTawfik22 <134923845+AhmedTawfik22@users.noreply.github.com>
2023-06-05 12:42:34 +0300

    Update index.js

--- react-redux-master/src/index.js
+++ react-redux-master/src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
-import {createStore} from 'redux';
-import {Provider} from 'react-redux'
-import myReducers from './reducers'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-
-//Create the store
-const myStore = createStore(myReducers);
-
-//This will console log the current state everytime the state changes
-myStore.subscribe(()=>console.log(myStore.getState()));
-
-//Enveloping the App inside the Provider, ensures that the states in the store are available
-//throughout the application
-ReactDOM.render(<Provider store={myStore}><App/></Provider>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
